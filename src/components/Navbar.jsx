1838752a8ff439af9f5ba23171639e774788e3b0
Abdul Muqsit <muqsit816@gmail.com>
2026-08-13 18:10:04 +0500

    update logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
         <div className="mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-14">
             {/* Logo */}
-            <a href="#" className="flex items-center flex-shrink-0">
+            <a href="#" className="flex items-center gap-1 flex-shrink-0">
               <img src={logo} alt="PrepNation Logo" className="h-8 w-auto rounded-md" />
               <span className="font-black text-xl tracking-tight" style={{ color: '#1E1B4B' }}>
                 Prep<span style={{ color: '#F59E0B' }}>Nation</span>
